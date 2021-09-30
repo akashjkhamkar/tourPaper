475f2f91402110219ad7cb87d6a235bec931196d
akash <akash.khamkar40@gmail.com>
2021-09-30 08:07:37 +0530

    desk env check fixed

--- tourPaperTask.py
+++ tourPaperTask.py
@@ -8,6 +8,21 @@ folder = "/tourpaper/"
 path = str(Path.home()) + folder
 linksFile = path + "/link.txt"
 
+def detect_desktop_environment():
+    desktop_environment = 'generic'
+    if os.environ.get('KDE_FULL_SESSION') == 'true':
+        desktop_environment = 'kde'
+    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+        desktop_environment = 'gnome'
+    else:
+        try:
+            info = getoutput('xprop -root _DT_SAVE_MODE')
+            if ' = "xfce4"' in info:
+                desktop_environment = 'xfce'
+        except (OSError, RuntimeError):
+            pass
+    return desktop_environment
+
 def main():
     if(os.path.exists(images) == False):
         os.mkdir(images)
@@ -34,9 +49,11 @@ def main():
         open(images+name, "wb").write(res.content)
 
         print("setting...", name)
-        envi = os.environ["XDG_SESSION_DESKTOP"]
+        envi = detect_desktop_environment();
         out = subprocess.check_output( "xrandr | grep ' connected ' | awk '{ print$1 }' " , shell=True)
         mon = out.decode("utf-8").strip()
+
+        print(envi)
 
         if envi == "gnome":
             os.system(f"gsettings set org.gnome.desktop.background picture-uri {images+name}")
